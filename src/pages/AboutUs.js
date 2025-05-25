79a0bd34603a5d5634472847eb1be53f748ced3b
snsh1n3 <fcc.95@hotmail.com>
2025-05-25 15:19:42 -0500

    v0.4.6.5

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,7 +9,7 @@ function AboutUs() {
           <p>Selvárea es una marca colombiana que celebra la riqueza natural del país a través de prendas únicas con bordados realistas de animales, hechos a mano por artesanos locales. Inspirada en la biodiversidad y los ecosistemas colombianos, Selvárea fusiona arte, sostenibilidad y diseño contemporáneo, creando piezas que son más que ropa: son un homenaje al patrimonio cultural y natural. Cada puntada cuenta una historia, destacando la conexión entre moda y conservación ambiental, mientras empodera a comunidades locales y preserva técnicas artesanales.</p>
         </div>
         <div className="image">
-          <img src="/Imagenes/colibriBordado.png" alt="Colibrí bordado a mano en una chaqueta" /> 
+          <img src={`${process.env.PUBLIC_URL}/Imagenes/colibriBordado.png`} alt="Colibrí bordado a mano en una chaqueta" /> 
         </div>
       </div>
     </section>
